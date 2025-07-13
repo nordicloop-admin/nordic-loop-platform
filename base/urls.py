bfd31b0d8368af431005213932da8e3c0cea745a
Karera-o <olivierkarera2020@gmail.com>
2025-07-13 20:53:46 +0200

    wip

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SystemStatsView, UserDashboardStatsView
+from .views import SystemStatsView, UserDashboardStatsView, TotalBidsCountView
 
 app_name = 'base'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     
     # User dashboard statistics endpoint
     path('dashboard/stats/', UserDashboardStatsView.as_view(), name='user-dashboard-stats'),
+    
+    # Total bids count endpoint
+    path('bids/count/', TotalBidsCountView.as_view(), name='total-bids-count'),
 ]
